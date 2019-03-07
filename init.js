1edd4327e7c78577fba8ce90ec88ad41f4ed9ee7
Glenn Vandeuren <vandeurenglenn@gmail.com>
2019-03-07 22:45:14 +0100

    :metal: don't build when initialized

--- init.js
+++ init.js
@@ -1,10 +1,19 @@
 const { spawn } = require('child_process');
+const { readFile } = require('fs');
 
 if (process.platform === 'win32') return;
-const child = spawn('npm', ['rebuild', '--build-from-source', 'grpc']);
+return readFile(join(__dirname, 'inited'), (err, data) => {
+  if (err) {
+    const child = spawn('npm', ['rebuild', '--build-from-source', 'grpc']);
 
-child.stdout.on('data', data => {
-  console.log(data.toString());
-});
+    child.stdout.on('data', data => {
+      console.log(data.toString());
+    });
 
-return child;
+    child.on('close', code => {
+      if (!code) spawn('touch', ['inited'])
+    })
+
+    return child;
+  }
+})
